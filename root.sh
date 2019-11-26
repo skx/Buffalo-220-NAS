c7067c5caf4447a1dcf45a46c14d4398970b04fb
CrashSerious <CrashSerious@gmail.com>
2019-11-26 15:47:18 -0600

    modifications to allow for sshd starting up to persist a reboot

--- root.sh
+++ root.sh
@@ -10,7 +10,7 @@
 ./nas "(echo ssh.pass;echo ssh.pass)|passwd"
 
 #
-#  2. Enable SFTP and confirm it is done
+#  2. Enable SFTP and confirm it is done (this is likely unneeded for ssh access)
 #
 ./nas "sed -i 's/SUPPORT_SFTP=0/SUPPORT_SFTP=1/g' /etc/nas_feature"
 ./nas "grep SFTP /etc/nas_feature"
@@ -22,7 +22,12 @@
 ./nas "grep Root /etc/sshd_config"
 
 #
-#  4. Enable SFTP
+#  4. Make Root persist a reboot, regardless of SUPPORT_SFTP
+#
+./nas "sed -i 's/ exit 0/ #exit 0/g' /etc/init.d/sshd.sh"
+
+#
+#  5. Enable sshd
 #
 ./nas "/etc/init.d/sshd.sh stop"
 ./nas "/etc/init.d/sshd.sh start"
